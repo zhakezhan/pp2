cb764cce682879e851b44887f84e20013f72eaaa
Nargiz <nargiz.zhakezhan@gmail.com>
2026-02-27 11:33:30 +0500

    Delete 111.py

--- 111.py
+++
@@ -1,9 +0,0 @@
-Your-Repository/ 
-├── generators.py     
-# Iterator and generator exercises 
-├── dates.py          
-# Date and time operations 
-├── math.py          
- # Math and random operations 
-└── json.py           
- # JSON parsing and creation
