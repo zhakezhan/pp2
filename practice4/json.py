9dce17b0417a7e5c8527421d5f6b9d8f7a80a3b6
ASUS <zhakezhan>
2026-02-27 11:30:51 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

---
+++ practice4/json.py
@@ -0,0 +1,68 @@
+#JSON is a syntax for storing and exchanging data.
+
+#1
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+#2
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
+
+#3
+import json
+
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+
+#4
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
+
+#5
+#Use the indent parameter to define the numbers of indents:
+#Use the separators parameter to change the default separator:
+json.dumps(x, indent=4, separators=(". ", " = "))
+
+#6
+json.dumps(x, indent=4, sort_keys=True)
